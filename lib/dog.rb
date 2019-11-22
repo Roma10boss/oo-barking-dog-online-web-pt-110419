05f8fd6d94679eb86919293e96748948c93bee17
Romario Gustave <romagustave10@gmail.com>
2019-11-22 13:40:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,3 +4,18 @@ class Dog
   fido = Dog.new 
   fido.name = "fido"
   fido.name 
+  
+  
+   def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
